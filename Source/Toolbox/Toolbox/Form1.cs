772337cb6fc7f5946f7693e3e514c671e13978cc
rprakitpong <rprakitpong@gmail.com>
2019-08-30 11:24:44 -0700

    generalized app by getting params from ModelWrapper instead of hardcode

--- Source/Toolbox/Toolbox/Form1.cs
+++ Source/Toolbox/Toolbox/Form1.cs
@@ -10,15 +10,27 @@ using System.Windows.Forms;
 
 namespace Toolbox
 {
-    using InventorUtilities;
-
     public partial class Form1 : Form
     {
-        ModelWrapper tm;
+        // model wrapper, interface to inventor window
+        ModelWrapper mw = new ModelWrapper();
 
-        double[] dims = { 0, 0, 0, 0, 0 }; 
-        // TODO length fixed at 5
-        // dimension order in array tightly coupled with CAD model dimension list
+        // unit/cm ratio table for conversion
+        Dictionary<string, double> unitsTable = new Dictionary<string, double>();
+
+        // unit fields
+        string unitName = "unit";
+        int unitFieldsCount = 3;
+        Dictionary<string, string> unitFieldTable = new Dictionary<string, string>();
+
+        // input array fields
+        string inputName = "val_dim";
+        string labelName = "name_dim";
+        int inputFieldsCount = 4;
+        Dictionary<string, TextBox> inputs = new Dictionary<string, TextBox>();
+
+        // form name
+        string formName = " Generater";
 
         public Form1()
         {
@@ -27,10 +39,64 @@ namespace Toolbox
 
         protected override void OnLoad(EventArgs e)
         {
-            tm = new ModelWrapper();
+            // initialize converter table
+            unitsTable.Add("cm", 1); // cm must be first, dependency with form design
+            unitsTable.Add("mm", 0.1);
+            unitsTable.Add("in", 2.54);
 
-            base.OnLoad(e); 
+            base.OnLoad(e);
+
+            initFormDimFields(mw.getDims());
+            initFormName(mw.getName());
+            initUnitButtons();
         }
+
+        private void initUnitButtons()
+        {
+            int i = 0;
+            RadioButton temp;
+            foreach (KeyValuePair<string, double> unit in unitsTable)
+            {
+                temp = (RadioButton)this.Controls.Find(unitName + i, true)[0];
+                if (temp != null)
+                {
+                    temp.Text = unit.Key;
+                    unitFieldTable.Add(temp.Name, unit.Key);
+                }
+                i++;
+            }
+            if (i < unitFieldsCount)
+            {
+                i--;
+                while (i + 1 < unitFieldsCount)
+                {
+                    i++;
+                    this.Controls.Find(unitName + i, true)[0].Visible = false;
+                }
+            }
+        }
+
+        private void initFormName(string v)
+        {
+            this.Text = v + formName;
+        }
+
+        private void initFormDimFields(string[] v)
+        {
+            for (int i = 0; i < inputFieldsCount; i++)
+            {
+                if (i < v.Length)
+                {
+                    inputs.Add(v[i], (TextBox)this.Controls.Find(inputName + i, true)[0]);
+                    this.Controls.Find(labelName + i, true)[0].Text = v[i];
+                } else
+                {
+                    this.Controls.Find(inputName + i, true)[0].Visible = false;
+                    this.Controls.Find(labelName + i, true)[0].Visible = false;
+                }
+            }
+        }
+
 
         /*
         protected override void Dispose(bool disposing)
@@ -38,18 +104,25 @@ namespace Toolbox
             base.Dispose(disposing);
         }
         */
+
         private void button1_Click(object sender, EventArgs e)
         {
-            try
+            var temp = from RadioButton r in unitsGroup.Controls where r.Checked == true select r.Name;
+            double unitConversion = unitsTable[unitFieldTable[temp.First()]];
+            Dictionary<string, double> dims = new Dictionary<string, double>();
+            foreach (KeyValuePair<string, TextBox> inputPair in inputs)
             {
-                dims[0] = Convert.ToDouble(diamText.Text);
-                dims[1] = Convert.ToDouble(thickText.Text);
-
-                tm.setDims(dims);
-            } catch (System.FormatException)
-            {
-                MessageBox.Show("Invalid input: please input number.");
+                try
+                {
+                    dims.Add(inputPair.Key, unitConversion * Convert.ToDouble(inputPair.Value.Text));
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("Invalid input: please input number.");
+                }
             }
+            mw.setDims(dims);
         }
+        
     }
 }
---
+++ Source/Toolbox/Toolbox/ModelWrapper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Toolbox
+{
+    using Inventor;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using System.Windows.Forms;
+
+    public class ModelWrapper
+    {
+        private Inventor.Application m_inventorInstance;
+        private PartDocument m_partInstance;
+        private UserParameters m_partParams;
+        private string[] m_partParamNames;
+
+        public ModelWrapper()
+        {
+            Console.WriteLine("loaded test model");
+
+            try
+            {
+                try
+                {
+                    // Get active inventor object
+                    m_inventorInstance = System.Runtime.InteropServices.Marshal.GetActiveObject("Inventor.Application") as Inventor.Application;
+                }
+                catch (COMException)
+                {
+                    MessageBox.Show("Inventor must be running.");
+                }
+
+                m_partInstance = m_inventorInstance.ActiveDocument as PartDocument;
+                if (m_partInstance == null)
+                {
+                    MessageBox.Show("Part from parts library must be opened");
+                }
+
+                // init partparams collection
+                m_partParams = m_partInstance.ComponentDefinition.Parameters.UserParameters;
+
+                // init partparams names array
+                IEnumerator partParamsEnum = m_partParams.GetEnumerator();
+                m_partParamNames = new string[m_partParams.Count - 1];
+                UserParameter temp;
+                int index = 0;
+                while (partParamsEnum.MoveNext())
+                {
+                    temp = partParamsEnum.Current as UserParameter;
+                    if (temp.Name != "type")
+                    {
+                        m_partParamNames[index] = temp.Name;
+                        index++;
+                    }
+                }
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public void setDims(Dictionary<string, double> nameDimsPair)
+        {
+            UserParameter temp;
+            foreach (KeyValuePair<string, double> pair in nameDimsPair)
+            {
+                temp = m_partParams[pair.Key];
+                if (temp == null)
+                {
+                    Console.WriteLine("dimension with name " + pair.Key + " not found in " + m_partParams["type"].Value);
+                } else
+                {
+                    temp.Value = pair.Value;
+                }
+            }
+        }
+
+        public string[] getDims()
+        {
+            return m_partParamNames;
+        }
+
+        public string getName()
+        {
+            return m_partParams["type"].Value as string;
+        }
+
+    }
+}
